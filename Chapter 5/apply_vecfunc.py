39e5f5fd7040483c72e3726102f72b1fcc372284
CLuan <congluan@zetamotion.com>
2024-07-02 18:06:19 +0700

    Add Python.pdf and Chapter 5 exercise 5.5

---
+++ Chapter 5/apply_vecfunc.py
@@ -0,0 +1,18 @@
+import math
+import numpy as np
+
+def f_list(x): return x**3+x*math.exp(x)+1
+
+#List method
+v = [2,3,-1]
+def f_list(x): return x**3+x*math.exp(x)+1
+y = [f_list(x) for x in v]
+print(y)
+
+
+
+# Array method
+v = (2,3,-1)
+def f_array(input): return np.array([x**3+x*math.exp(x)+1 for x in input])    # Convert to array
+print(f_array(v))
+
