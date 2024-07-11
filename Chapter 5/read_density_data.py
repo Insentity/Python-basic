9a884d28c5154cb49fa947e736d6a41dc7f9f74a
CLuan <congluan@zetamotion.com>
2024-07-11 14:34:14 +0700

    Update

--- Chapter 5/read_density_data.py
+++ Chapter 5/read_density_data.py
@@ -3,37 +3,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def read_dat_file(path):
-    df = pd.read_csv(path)
-    df = df.iloc[2:11].iloc[:,0:1]
-    df = df.iloc[:,0].tolist()
-    # print(df)
-    # df = df.tolist()
+def read_dat_file(path,length):     # Path is the path to the .dat file, length is the number of rows that contain the density values
+    df = pd.read_csv(path)      # Convert to pandas dataframe
+    df = df.iloc[2:length+2].iloc[:,0:1]
+    # iloc[2:length+2] slice df from row 2 to row length+2, starts from 2 to remove the headings. length+2 since we starts at index 2
+    # iloc[:,0:1] slide the resulting df to only column 0
+    df = df.iloc[:,0].tolist()  # Convert dataframe to python list
     return(df)
 
 
 def filter(nparray):
     # print(nparray[4])
-    temperature = np.zeros(len(nparray))
+    temperature = np.zeros(len(nparray))    # Initialize np array
     density = np.zeros(len(nparray))
     for count,data in enumerate(nparray):
-        # print(type(data))
-        temperature[count] = data.split("     ")[0]
+        print(data.split("     ")[0])   # Split the temperature values from density
+        temperature[count] = data.split("     ")[0] # Obtain the temperature value from the list
         density[count] = data.split("     ")[1]
     return(temperature, density)
 
 
-def plot_density(x,y):
+def plot_density(x,y, title):
+    plt.figure()
     plt.plot(x, y)
     plt.xlabel("Temperature (C)")
     plt.ylabel("Density (kg/m3)")
-    plt.show()
+    plt.title(title)
+    
 
 
 
-air = read_dat_file("Chapter 5\density_air.dat")
-# water = read_dat_file("Chapter 5\density_water.dat")
+air = read_dat_file("Chapter 5\density_air.dat", 9)
+water = read_dat_file("Chapter 5\density_water.dat", 8)
 # print(len(air))
 air_temperature, air_density = filter(air)
-# water_temperature, water_density = filter(water)
-plot_density(air_temperature, air_density)
+water_temperature, water_density = filter(water)
+plot_density(air_temperature, air_density, "Air density vs Temperature")
+plot_density(water_temperature, water_density, "Water density vs Temperature")
+plt.show()
+
